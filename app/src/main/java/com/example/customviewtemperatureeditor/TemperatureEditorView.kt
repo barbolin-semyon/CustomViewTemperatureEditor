2f5d79fe7968e551676fc26ad5560185abc6f4f1
syoma <skat.apps@gmail.com>
2022-02-28 01:02:31 +0300

    fix: dinamic size

--- app/src/main/java/com/example/customviewtemperatureeditor/TemperatureEditorView.kt
+++ app/src/main/java/com/example/customviewtemperatureeditor/TemperatureEditorView.kt
@@ -3,17 +3,14 @@ package com.example.customviewtemperatureeditor
 import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.*
-import android.graphics.drawable.BitmapDrawable
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
-import kotlin.properties.Delegates
 
 class TemperatureEditorView(
     context: Context,
-    attributeSet: AttributeSet?,
-    defStyleAttr: Int,
-    defStyleRes: Int
+    private val attributeSet: AttributeSet?,
+    private val defStyleAttr: Int,
+    private val defStyleRes: Int
 ) : View(context, attributeSet, defStyleAttr, defStyleRes) {
 
     constructor(context: Context, attributeSet: AttributeSet?, defStyleAttr: Int)
@@ -25,20 +22,22 @@ class TemperatureEditorView(
     constructor(context: Context)
             : this(context, null)
 
-
-    init {
-        if (attributeSet != null) {
-            initAttributes(attributeSet, defStyleAttr, defStyleRes)
-        }
-        initPaint()
-    }
-
     private lateinit var paintBackground: Paint
     private lateinit var paintBackgroundCenter: Paint
     private lateinit var paintProgress: Paint
     private lateinit var paintSlider: Paint
 
-    private val reactProgress = RectF(300f, 300f, 700f, 700f)
+    private var outerRadius = 0f
+    private var progressRadius = 0f
+    private var innerRadius = 0f
+    private var centerX = 0f
+    private var centerY = 0f
+
+    private var widthSlider = 1.7f
+    private var heightSlider = 60f
+    private var widthProgress = 25f
+
+    private lateinit var reactProgress: RectF
 
     private var colorBackgroundCircle: Int = DEFAULT_COLOR_BACKGROUND
     private var colorBackgroundCenterStart = DEFAULT_COLOR_BACKGROUND_CENTRE_START
@@ -46,6 +45,8 @@ class TemperatureEditorView(
     private var colorProgressStart = DEFAULT_COLOR_PROGRESS_START
     private var colorProgressEnd = DEFAULT_COLOR_PROGRESS_END
     private var colorSlider = DEFAULT_COLOR_SLIDER
+
+    private var progress = 0f
 
     @SuppressLint("ResourceAsColor")
     private fun initPaint() {
@@ -60,15 +61,15 @@ class TemperatureEditorView(
     private fun initPaintProgress() {
         paintProgress = Paint(Paint.ANTI_ALIAS_FLAG).apply {
             style = Paint.Style.STROKE
-            strokeWidth = 25F
+            strokeWidth = widthProgress
             strokeCap = Paint.Cap.ROUND
 
             val matrix = Matrix().apply {
-                setRotate(45F, 500F, 500F)
+                setRotate(45F, centerX, centerY)
             }
 
             val gradient = SweepGradient(
-                500f, 500f,
+                centerX, centerY,
                 IntArray(3).apply {
                     this[0] = colorProgressStart
                     this[1] = colorProgressEnd
@@ -83,10 +84,10 @@ class TemperatureEditorView(
 
     private fun initPaintBackgroundCenter() {
 
-        val matrix = Matrix().apply { setRotate(-115f, 500f, 500f) }
+        val matrix = Matrix().apply { setRotate(-115f, centerX, centerY) }
 
         val gradient = SweepGradient(
-            500f, 500f,
+            centerX, centerY,
             IntArray(3).apply {
                 this[0] = colorBackgroundCenterStart
                 this[1] = colorBackgroundCenterEnd
@@ -105,16 +106,53 @@ class TemperatureEditorView(
             strokeCap = Paint.Cap.ROUND
             color = colorSlider
             style = Paint.Style.STROKE
-            strokeWidth = 60f
+            strokeWidth = heightSlider
         }
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        canvas!!.drawCircle(500f, 500f, 240f, paintBackground)
-        canvas.drawCircle(500f, 500f, 187f, paintBackgroundCenter)
-        canvas.drawArc(reactProgress, 0f, 270f, false, paintProgress)
-        canvas.drawArc(reactProgress, 270f, 1.7f, false, paintSlider)
+        progress++
+        canvas!!.drawCircle(centerX, centerY, progressRadius, paintBackground)
+        canvas.drawCircle(centerX, centerY, innerRadius, paintBackgroundCenter)
+        canvas.drawArc(reactProgress, 0f, progress % 360, false, paintProgress)
+        canvas.drawArc(reactProgress, progress % 360, widthSlider, false, paintSlider)
+
+        invalidate()
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+
+        outerRadius = (w - paddingLeft - paddingRight) / 2f
+
+        widthProgress = outerRadius / 10f
+        heightSlider = widthProgress * 1.3f
+        heightSlider = widthProgress * 1.3f
+
+        val widthOuterBackground = outerRadius / 2f
+
+
+        progressRadius = outerRadius - widthOuterBackground
+        innerRadius = progressRadius - widthProgress
+
+        centerX = paddingLeft + outerRadius
+        centerY = paddingTop + outerRadius
+
+
+        reactProgress = RectF(
+            centerX - progressRadius + widthProgress,
+            centerY - progressRadius + widthProgress,
+            centerX + progressRadius - widthProgress,
+            centerY + progressRadius - widthProgress
+        )
+
+        if (attributeSet != null) {
+            initAttributes(attributeSet, defStyleAttr, defStyleRes)
+        }
+
+        initPaint()
     }
 
     @SuppressLint("CustomViewStyleable")
